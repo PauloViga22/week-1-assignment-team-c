929300463d8d20695ba39cb0dd75f33c4d2cf21a
Francois Polo <andasan@gmail.com>
2020-01-09 15:10:39 -0800

    add views and new routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,14 +4,20 @@ const router = express.Router();
 const data = [];
 
 router.get('/', (req,res,next) => {
-    res.render('index', {notes: data});
+    res.send('index', {notes: data});
     console.log(data);
+});
+
+router.get('/create', (req,res,next) => {
+    res.send('create');
+});
+
+router.get('/list', (req,res,next) => {
+    res.render('list');
 });
 
 router.post('/note', (req,res,next) =>{
 
-    
-    
     let myNewEntry = {
         title: req.body.title,
         content: req.body.body,
