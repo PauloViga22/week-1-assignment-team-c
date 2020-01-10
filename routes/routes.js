f1187672b2f8b49a453d11fed72ca0dd8ed9b546
Francois Polo <andasan@gmail.com>
2020-01-10 15:58:00 -0800

    latest master

--- routes/routes.js
+++ routes/routes.js
@@ -4,15 +4,7 @@ const fs = require("fs");
 const router = express.Router();
 
 const { appendFile, readFile } = require("../utlils");
-const data = [
-  // {
-  //   name: "das",
-  //   content: "das",
-  //   published: "2020-01-09T23:59:05.924Z",
-  //   upvotes: 0,
-  //   downvotes: 0
-  // }
-];
+const data = [];
 let options = {
   weekday: "long",
   year: "numeric",
@@ -28,48 +20,41 @@ router.get("/create", (req, res, next) => {
   res.sendFile(path.join(__dirname, "../views", "create.html"));
 });
 
-router.get("/list", async (req, res, next) => {
-  let rowFileData = "";
-  fs.readFile("notes.txt", "utf8", async (err, data) => {
-    //エラーの場合はエラーを投げてくれる
-    if (err) {
-      throw err;
-    }
-    rowFileData = data;
-    if (rowFileData.slice(0, 1) === ".") {
-      const arrData = rowFileData.slice(1).split(".");
-      // const arrData = fileData.split(".");
-      if (!arrData.length) {
-        res.render("list", { notes: [] });
-      } else {
-        res.render("list", {
-          notes: arrData
-        });
+router.get("/list", (req, res, next) => {
+    fs.readFile("notes.txt", "utf-8", (err, data) => {
+      let notes = [];
+      if (!err) {
+        try {
+          notes = JSON.parse(data);
+          // console.log("what is this oneeeeeeee", notes);
+        } catch (e) {
+          fs.writeFileSync("notes.txt", []);
+          notes = [];
+        }
       }
-    }
+      data = notes
+      res.render('list', {
+        notes: data
+      });
+    });
   });
-});
 
-router.post("/note", (req, res, next) => {
-  let myNewEntry = {
-    name: req.body.name,
-    content: req.body.body,
-    published: new Date(),
-    upvotes: 0,
-    downvotes: 0
-  };
+router.post('/note', (req, res, next) => {
+    let myNewEntry = {
+      name: req.body.name,
+      content: req.body.body,
+      published: new Date(),
+      upvotes: 0,
+      downvotes: 0
+    };
+    console.log("old data", data)
+    data.push(myNewEntry);
+    console.log("new data", data)
+    fs.writeFile('notes.txt', JSON.stringify(data), () => {
+      res.status(302);
+      res.redirect('/');
+    });
+  });
 
-  // data.push(myNewEntry);
-
-  appendFile("notes.txt", `.${JSON.stringify(myNewEntry)}`);
-
-  // fs.writeFile("notes.txt", JSON.stringify(data), () => {
-  //   res.status(302);
-  //   res.redirect("/");
-  // });
-
-  res.status(302);
-  res.redirect("/");
-});
 
 module.exports = router;
