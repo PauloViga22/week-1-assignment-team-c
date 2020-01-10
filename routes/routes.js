852fb2ca52dd2ea4f55987c824774649fc43b014
Francois Polo <andasan@gmail.com>
2020-01-10 14:32:04 -0800

    date formatting applicable on ejs

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const router = express.Router();
-var data = [{"name":"das","content":"das","published":"2020-01-09T23:59:05.924Z","upvotes":0,"downvotes":0},{"name":"Penny","content":"Make me toss a coin to my Witcher","published":"Friday, January 10, 2020","upvotes":0,"downvotes":0},{"name":"Clark Kent","content":"A new shirt","published":"Friday, January 10, 2020","upvotes":0,"downvotes":0},{"name":"Cookie monster","content":"COOOKKKIIIIEEEEE!!!","published":"Friday, January 10, 2020","upvotes":0,"downvotes":0},{"name":"Elsa","content":"I wish to build a snowman","published":"Friday, January 10, 2020","upvotes":0,"downvotes":0},{"name":"Homer","content":"I wish for a six pack......of BEER!","published":"Friday, January 10, 2020","upvotes":0,"downvotes":0},{"name":"Kiddo","content":"World Peace","published":"Friday, January 10, 2020","upvotes":0,"downvotes":0}];
+var data = [];
 let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 router.get('/', (req, res, next) => {
@@ -23,7 +23,7 @@ router.post('/note', (req, res, next) => {
   let myNewEntry = {
     name: req.body.name,
     content: req.body.body,
-    published: new Date().toLocaleDateString("en-US", options),
+    published: new Date(),
     upvotes: 0,
     downvotes: 0
   };
